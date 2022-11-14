370ccf42dab9df4c10018d26088dfb05ebea203b
Brandon Lowe <brandon.ldata@gmail.com>
2022-11-14 17:01:50 +1000

    init commit

---
+++ src/dependencies.jl
@@ -0,0 +1,5 @@
+using DataFrames, CSV, Plots, StatsPlots, StatsBase, HTTP, SplitApplyCombine
+
+include("helper-functions.jl") # some helper functions I wrote to help keep notebook clean
+
+housing_data_df = import_data("data/Melbourne_housing_FULL.csv")
