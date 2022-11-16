d3f8a7a540d10898cb87ab574850eb63bceebb6e
Brandon Lowe <brandon.ldata@gmail.com>
2022-11-16 14:46:30 +1000

    update: Task 1 complete

--- src/dependencies.jl
+++
@@ -1,5 +0,0 @@
-using DataFrames, CSV, Plots, StatsPlots, StatsBase, HTTP, SplitApplyCombine, GLM, Random
-
-include("helper-functions.jl") # some helper functions I wrote to help keep notebook clean
-
-housing_data_df = import_data("data/Melbourne_housing_FULL.csv")
